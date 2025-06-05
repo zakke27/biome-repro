d05e4fa64cecf8a002ac7bef26f97afe380abfa2
Zakke <zakkeke27@gmail.com>
2025-06-05 17:24:52 +0800

    init

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * If possible, put your minimal reproduction in this file, but feel free to
- * create other files elsewhere in the project if necessary.
- *
- * Once you have a minimal reproduction, commit it and push it to a public
- * GitHub repo.
- */
